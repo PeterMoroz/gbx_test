daefb8c1515804008c5783d655aa6410a28c4f90
Peter Moroz <ptr.moroz@gmail.com>
2022-12-27 21:48:06 +0300

    refactoring of application: write log into file instead of stdout/stderr

--- src/application/application.cpp
+++ src/application/application.cpp
@@ -1,58 +1,42 @@
 #include <application.h>
 #include <handlers_factory.h>
 
-#include <iostream>
 
 #include <Poco/Net/HTTPServer.h>
 #include <Poco/Net/ServerSocketImpl.h>
 
+#include <Poco/Logger.h>
+#include <Poco/SimpleFileChannel.h>
+#include <Poco/FormattingChannel.h>
+#include <Poco/PatternFormatter.h>
+#include <Poco/AutoPtr.h>
 
-namespace gbx
-{
 
-class ServerSocketImpl : public Poco::Net::ServerSocketImpl
-{
-public:
-    using Poco::Net::SocketImpl::init;
-};
-
-class Socket : public Poco::Net::Socket
-{
-public:
-    Socket(const std::string& address)
-        : Poco::Net::Socket(new ServerSocketImpl())
-    {
-        const Poco::Net::SocketAddress socket_address(address);
-        ServerSocketImpl* socket = static_cast<ServerSocketImpl*>(impl());
-        socket->init(socket_address.af());
-        socket->setReuseAddress(true);
-        socket->setReusePort(false);
-        socket->bind(socket_address, false);
-        socket->listen();
-    }
-
-};
-
-}
 
 using namespace gbx;
 
 int Application::main(const std::vector<std::string>& )
 {
-    // Poco::Net::HTTPServerParams::Ptr parameters = new Poco::Net::HTTPServerParams();
-    // parameters->setTimeout(10000);
-    // parameters->setMaxQueued(100);
-    // parameters->setMaxThreads(4);
+    Poco::AutoPtr<Poco::SimpleFileChannel> simpleFileChannel(new Poco::SimpleFileChannel());
+    simpleFileChannel->setProperty("path", "gbx_test_service.log");
+    simpleFileChannel->setProperty("rotation", "2 K");
 
-    // const Poco::Net::ServerSocket socket(Socket("localhost:8000"));
-    // Poco::Net::HTTPServer server(new RequestHandlerFactory(), socket, parameters);
+    Poco::AutoPtr<Poco::PatternFormatter> patternFormatter(new Poco::PatternFormatter());
+    patternFormatter->setProperty("pattern", "[%p] %Y-%m-%d %H:%M:%S: %t");
 
-    // std::cout << "Start HTTP server on port 8000." << std::endl;
-    // server.start();
-    // waitForTerminationRequest();
-    // server.stopAll();
+    Poco::AutoPtr<Poco::FormattingChannel> formattingChannel(
+        new Poco::FormattingChannel(patternFormatter, simpleFileChannel));
 
-    Poco::Net::HTTPServer server(new RequestHandlerFactory(), 8000);
+    logger().setChannel(formattingChannel);
+
+    Poco::Net::HTTPServerParams::Ptr parameters = new Poco::Net::HTTPServerParams();
+    parameters->setTimeout(10000);
+    parameters->setMaxQueued(100);
+    parameters->setMaxThreads(4);
+
+    const Poco::Net::ServerSocket socket(8000);
+    Poco::Net::HTTPServer server(new RequestHandlerFactory(), socket, parameters);
+
     server.start();
     logger().information("HTTP server started on port 8000.");
     waitForTerminationRequest();
